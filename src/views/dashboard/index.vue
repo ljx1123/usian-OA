2716e0bca1c6772fbeadb8d61c9023693fbf3f48
ljxsdsfafafa <2636013359@qq.com>
2024-01-18 19:11:40 +0800

    init

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- 使用svg图标 -->
+    <svg-icon icon-class="eye"></svg-icon>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vue'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters(['name'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
